69dc918a53e65de908cf977daf362a0f0019bc71
butterbroad3000 <jekagonch@mail.ru>
2024-04-09 16:36:48 +0300

    first comm

---
+++ src/main/java/com/example/bankcourse/BankCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCourseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BankCourseApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/bankcourse/BankCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCourseApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
