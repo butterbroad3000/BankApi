007bdf19b1615010cfaf3199b6c3d6cf368c75d0
butterbroad3000 <jekagonch@mail.ru>
2024-04-09 20:06:57 +0300

    fix checkstyle

--- src/main/java/com/example/bankcourse/CurrencyService.java
+++ src/main/java/com/example/bankcourse/CurrencyService.java
@@ -6,33 +6,29 @@ import com.example.bankcourse.Models.Rate;
 import com.example.bankcourse.Models.Request.RateDynamicsRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class CurrencyService {
   private final NbrbClient nbrbClient;
 
   public List<Rate> getRateDynamics(RateDynamicsRequest request) {
-    // Получаем все валюты на начало интересующего периода
+
     List<Currency> currencies = nbrbClient.getCurrencies();
 
-    // Находим объект, описывающий интересующую нас валюту
     Currency currency = currencies.stream()
       .filter(c -> c.Cur_Abbreviation().equals(request.currencyAbbreviation()))
       .findFirst()
       .orElseThrow(() -> new IllegalArgumentException("No such currency abbreviation: " + request.currencyAbbreviation()));
 
-    // Находим все объекты, которые относятся к искомой валюте
     List<Currency> relevantCurrencies = currencies.stream()
-      .filter(c -> c.Cur_ParentID()==currency.Cur_ID())
+      .filter(c -> c.Cur_ParentID() == currency.Cur_ID())
       .toList();
 
     List<Rate> rates = new ArrayList<>();
 
-    // Получаем данные о динамике курса валюты для каждого из найденных объектов с учетом периода их действия
     for (Currency relevantCurrency : relevantCurrencies) {
       rates.addAll(nbrbClient.getRateDynamics(relevantCurrency.Cur_ID(), request.startDate(), request.endDate()));
     }
--- src/main/java/com/example/bankcourse/Models/Rate.java
+++ src/main/java/com/example/bankcourse/Models/Rate.java
@@ -1,8 +1,6 @@
 package com.example.bankcourse.Models;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 public record Rate(
   int Cur_ID,
