98422a861964c9223baa2fce309c6aa0ea9b6f70
butterbroad3000 <jekagonch@mail.ru>
2024-04-09 20:05:19 +0300

    worked

--- src/main/java/com/example/bankcourse/CurrencyService.java
+++ src/main/java/com/example/bankcourse/CurrencyService.java
@@ -7,7 +7,9 @@ import com.example.bankcourse.Models.Request.RateDynamicsRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class CurrencyService {
@@ -21,11 +23,20 @@ public class CurrencyService {
     Currency currency = currencies.stream()
       .filter(c -> c.Cur_Abbreviation().equals(request.currencyAbbreviation()))
       .findFirst()
-      .orElseThrow(() -> new IllegalArgumentException("Invalid currency abbreviation: " + request.currencyAbbreviation()));
+      .orElseThrow(() -> new IllegalArgumentException("No such currency abbreviation: " + request.currencyAbbreviation()));
+
+    // Находим все объекты, которые относятся к искомой валюте
+    List<Currency> relevantCurrencies = currencies.stream()
+      .filter(c -> c.Cur_ParentID()==currency.Cur_ID())
+      .toList();
+
+    List<Rate> rates = new ArrayList<>();
 
     // Получаем данные о динамике курса валюты для каждого из найденных объектов с учетом периода их действия
-    return nbrbClient.getRateDynamics(currency.Cur_ID(), request.startDate(), request.endDate());
+    for (Currency relevantCurrency : relevantCurrencies) {
+      rates.addAll(nbrbClient.getRateDynamics(relevantCurrency.Cur_ID(), request.startDate(), request.endDate()));
+    }
+
+    return rates;
   }
 }
-
-
--- src/main/java/com/example/bankcourse/Models/Rate.java
+++ src/main/java/com/example/bankcourse/Models/Rate.java
@@ -1,13 +1,11 @@
 package com.example.bankcourse.Models;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public record Rate(
   int Cur_ID,
-  Date date,
-  String Cur_Abbreviation,
-  int Cur_Scale,
-  String Cur_Name,
-  BigDecimal Cur_OfficialRate
-) {}
+  LocalDateTime Date,
+  double Cur_OfficialRate) {
+}
